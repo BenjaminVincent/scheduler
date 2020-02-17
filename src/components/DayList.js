11e1e6ffc2ff10d1ccc0e308373b177377954f76
BenjaminVincent <ben.tom.vincent@gmail.com>
2020-02-17 14:45:48 -0800

    finished DayList component

---
+++ src/components/DayList.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./DayListItem.scss";
+import DayListItem from "./DayListItem";
+
+
+
+export default function DayList(props) {
+  // deconstructs and grabs values!
+  const {days, setDay} = props;
+  return (
+    <ul> 
+      {
+        days.map((day, key) => 
+        <DayListItem 
+          name={day.name} 
+          spots={day.spots} 
+          selected={day.name === props.day}
+          setDay={setDay}
+          key={key}  />)
+      }
+    </ul>
+  );
+};
