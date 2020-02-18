5b5949b54c51737a6f347187ad411be725acf708
BenjaminVincent <ben.tom.vincent@gmail.com>
2020-02-18 11:14:58 -0800

    now displaying all static appointments

--- stories/index.js
+++ stories/index.js
@@ -137,6 +137,50 @@ storiesOf("DayListItem", module)
             onChange={action("onChange")}
           />
         ));
+
+
+        const appointments = [
+          {
+            id: 1,
+            time: "12pm",
+          },
+          {
+            id: 2,
+            time: "1pm",
+            interview: {
+              student: "Lydia Miller-Jones",
+              interviewer: {
+                id: 1,
+                name: "Sylvia Palmer",
+                avatar: "https://i.imgur.com/LpaY82x.png",
+              }
+            }
+          }, 
+          {
+            id: 3,
+            time: "2pm",
+            interview: {
+              student: "Benjamin Vincent",
+              interviewer: {
+                id: 2,
+                name: "Tori Malcolm",
+                avatar: "https://i.imgur.com/Nmx0Qxo.png"
+              }
+            }
+          },
+          {
+            id: 4,
+            time: "3pm",
+            interview: {
+              student: "Mike Thomerson",
+              interviewer: {
+                id: 3,
+                name: "Mildred Nazir",
+                avatar: "https://i.imgur.com/T2WwVfS.png"
+              }
+            }
+          }
+        ];
   
 
 
