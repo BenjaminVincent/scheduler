1473bffb3915ffdc62857458af480f11edb1c2cf
BenjaminVincent <ben.tom.vincent@gmail.com>
2020-02-19 09:16:23 -0800

    fixed selectors.js bug

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,37 +1,19 @@
 
-const state = {
-  days: [
-    {
-      id: 1,
-      name: "Monday",
-      appointments: [1, 2, 3]
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      appointments: [4, 5]
-    }
-  ],
-  appointments: {
-    "1": { id: 1, time: "12pm", interview: null },
-    "2": { id: 2, time: "1pm", interview: null },
-    "3": {
-      id: 3,
-      time: "2pm",
-      interview: { student: "Archie Cohen", interviewer: 2 }
-    },
-    "4": { id: 4, time: "3pm", interview: null },
-    "5": {
-      id: 5,
-      time: "4pm",
-      interview: { student: "Chad Takahashi", interviewer: 2 }
+export function getAppointmentsForDay (state, day) {
+  const fetchedApp = [];
+  const dayObj = state.days.filter(d => d.name === day)[0];
+  if (state.days < 1) {
+    return [];
+  }
+
+  if (!dayObj) {
+    return [];
+  }
+
+  for (const key of dayObj.appointments) {
+    if (state.appointments[key]) {
+      fetchedApp.push(state.appointments[key]);
     }
   }
-};
-
-
-function getAppointmentsForDay (state, day) {
-  return [];
+  return fetchedApp;
 }
-
-console.log(getAppointmentsForDay(state, "Monday"));
