029486945d627e02dd3ca1cb499f81e8902cd29c
BenjaminVincent <ben.tom.vincent@gmail.com>
2020-02-19 10:00:58 -0800

    updated Application states

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,66 +3,31 @@ import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment"
+import { getAppointmentsForDay } from "../helpers/selectors.js";
 
-
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  }, 
-  {
-    id: 3,
-    time: "2pm",
-    interview: {
-      student: "Benjamin Vincent",
-      interviewer: {
-        id: 2,
-        name: "Tori Malcolm",
-        avatar: "https://i.imgur.com/Nmx0Qxo.png"
-      }
-    }
-  },
-  {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Mike Thomerson",
-      interviewer: {
-        id: 3,
-        name: "Mildred Nazir",
-        avatar: "https://i.imgur.com/T2WwVfS.png"
-      }
-    }
-  },
-  {
-    id: 5,
-    time: "3pm",
-    interview: {
-      student: "Carl Winkleton",
-      interviewer: {
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg"
-      }
-    }
-  }
-];
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  // const [day, setDay] = useState("Monday");
+  // const [days, setDays] = useState([]);
+
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+
+  const setDay = day =>
+  setState({
+    ...state,
+    day
+  });
+
+const setDays = days =>
+  setState(prev => ({
+    ...prev,
+    days
+  }));
+
 
   useEffect(() => {
     axios
@@ -72,7 +37,7 @@ export default function Application(props) {
     .catch((error) => {
       console.log(error.response.status);
     });
-  })
+  });
   
   return (
     <main className="layout">
@@ -85,8 +50,8 @@ export default function Application(props) {
       <hr className="sidebar__separator sidebar--centered" />
       <nav className="sidebar__menu">
       <DayList
-        days={days}
-        day={day}
+        days={state.days}
+        day={state.day}
         setDay={setDay} />
       </nav>
       <img  
@@ -96,13 +61,67 @@ export default function Application(props) {
       />
       </section>
       <section className="schedule">
-      {
-        appointments.map((appointment) =>
-      <Appointment key={appointment.id} {...appointment} /> 
-       )
-      }
+      {getAppointmentsForDay(state, state.day).map(app =>
+          <Appointment {...app} key={app.id} />
+        )}
       <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
 }
+
+
+// const appointments = [
+//   {
+//     id: 1,
+//     time: "12pm",
+//   },
+//   {
+//     id: 2,
+//     time: "1pm",
+//     interview: {
+//       student: "Lydia Miller-Jones",
+//       interviewer: {
+//         id: 1,
+//         name: "Sylvia Palmer",
+//         avatar: "https://i.imgur.com/LpaY82x.png",
+//       }
+//     }
+//   }, 
+//   {
+//     id: 3,
+//     time: "2pm",
+//     interview: {
+//       student: "Benjamin Vincent",
+//       interviewer: {
+//         id: 2,
+//         name: "Tori Malcolm",
+//         avatar: "https://i.imgur.com/Nmx0Qxo.png"
+//       }
+//     }
+//   },
+//   {
+//     id: 4,
+//     time: "3pm",
+//     interview: {
+//       student: "Mike Thomerson",
+//       interviewer: {
+//         id: 3,
+//         name: "Mildred Nazir",
+//         avatar: "https://i.imgur.com/T2WwVfS.png"
+//       }
+//     }
+//   },
+//   {
+//     id: 5,
+//     time: "3pm",
+//     interview: {
+//       student: "Carl Winkleton",
+//       interviewer: {
+//         id: 4,
+//         name: "Cohana Roy",
+//         avatar: "https://i.imgur.com/FK8V841.jpg"
+//       }
+//     }
+//   }
+// ];
