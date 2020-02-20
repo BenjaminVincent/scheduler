f1f4d95cdc5e0ba949c7333b3f58a559b35480ad
BenjaminVincent <ben.tom.vincent@gmail.com>
2020-02-20 14:32:14 -0800

    appointments now  delete upon clicking delete

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,6 +4,7 @@ import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
 import Form from "./Form";
+import Status from "./Status";
 import axios from "axios";
 // import { action } from "@storybook/addon-actions";
 import { useVisualMode } from "../../hooks/useVisualMode";
@@ -11,6 +12,8 @@ import { useVisualMode } from "../../hooks/useVisualMode";
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREATE";
+const SAVING = "SAVING";
+const DELETING = "DELETING";
 
 
 export default function Appointment(props) {
@@ -24,13 +27,13 @@ export default function Appointment(props) {
     };
 
     function onSave(name, interviewer) {
+      transition(SAVING);
 
       const interview = {
         student: name,
         interviewer
       };
       props.bookInterview(props.id, interview)
-
 
       axios
       .put(`/api/appointments/${props.id}`, {
@@ -39,6 +42,17 @@ export default function Appointment(props) {
       .then((response) => transition(SHOW))
       .catch(error => console.log(error.response.status));
     }
+
+    function onDelete(interview) {
+      transition(DELETING);
+
+      axios
+      .delete(`/api/appointments/${props.id}`, {
+        interview
+      })
+      .then((response) => transition(EMPTY))
+      .catch(error => console.log(error.response.status));
+    };
   
     function onCancel() {
       back();
@@ -48,10 +62,13 @@ export default function Appointment(props) {
       <article className="appointment">
         <Header time={props.time} />
         {mode === EMPTY && <Empty onAdd={onAdd} />}
+        {mode === SAVING && <Status message="Saving"/>}
+        {mode === DELETING && <Status message="Deleting"/>}
         {mode === SHOW && (
           <Show
             student={props.interview.student}
             interviewer={props.interview.interviewer}
+            onDelete={onDelete}
           />
         )} 
         {mode === CREATE && (
