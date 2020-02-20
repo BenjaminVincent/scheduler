eec1d3f0eb4fbfa043a622d8f3fdd97c8d9fbe33
BenjaminVincent <ben.tom.vincent@gmail.com>
2020-02-20 15:03:17 -0800

    appointments can be deleted now with a confirm state

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,7 +5,9 @@ import Show from "./Show";
 import Empty from "./Empty";
 import Form from "./Form";
 import Status from "./Status";
+import Confirm from "./Confirm";
 import axios from "axios";
+
 // import { action } from "@storybook/addon-actions";
 import { useVisualMode } from "../../hooks/useVisualMode";
 
@@ -14,6 +16,7 @@ const SHOW = "SHOW";
 const CREATE = "CREATE";
 const SAVING = "SAVING";
 const DELETING = "DELETING";
+const CONFIRM = "CONFIRM";
 
 
 export default function Appointment(props) {
@@ -33,7 +36,7 @@ export default function Appointment(props) {
         student: name,
         interviewer
       };
-      props.bookInterview(props.id, interview)
+      props.bookInterview(props.id, interview);
 
       axios
       .put(`/api/appointments/${props.id}`, {
@@ -43,10 +46,14 @@ export default function Appointment(props) {
       .catch(error => console.log(error.response.status));
     }
 
+    function waitForConfirm() {
+      transition(CONFIRM);
+    }
+
     function onDelete(interview) {
       transition(DELETING);
-
       axios
+      
       .delete(`/api/appointments/${props.id}`, {
         interview
       })
@@ -64,11 +71,16 @@ export default function Appointment(props) {
         {mode === EMPTY && <Empty onAdd={onAdd} />}
         {mode === SAVING && <Status message="Saving"/>}
         {mode === DELETING && <Status message="Deleting"/>}
+        {mode === CONFIRM && (
+        <Confirm 
+          message="Are you sure you would like to delete your appointment?"
+          onCancel={onCancel}
+          onConfirm={onDelete}/>)}
         {mode === SHOW && (
           <Show
             student={props.interview.student}
             interviewer={props.interview.interviewer}
-            onDelete={onDelete}
+            onDelete={waitForConfirm}
           />
         )} 
         {mode === CREATE && (
