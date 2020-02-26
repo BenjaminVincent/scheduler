c88068f9951cbd7d6a619708fe4439fe80e73c7f
BenjaminVincent <ben.tom.vincent@gmail.com>
2020-02-26 12:23:19 -0800

    moved reducer into reducers/application.js

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,48 +1,11 @@
 import { useReducer, useEffect } from "react";
-import { getSpotsForDay } from "helpers/selectors";
 import axios from "axios";
+import reducer, {
+  SET_DAY,
+  SET_APPLICATION_DATA,
+  SET_INTERVIEW
+} from "reducers/application";
 
-
-// Action types
-const SET_DAY = 'SET_DAY';
-const SET_APPLICATION_DATA = 'SET_APPLICATION_DATA';
-const SET_INTERVIEW = 'SET_INTERVIEW';
-
-function reducer(state, action) {
-  switch (action.type) {
-    case SET_DAY:
-      return { ...state, ...action.value };
-    case SET_APPLICATION_DATA:
-      return { ...state, ...action.value };
-    case SET_INTERVIEW: {
-      const { id, interview, onUpdate } = action.value;
-      const appointment = {
-        ...state.appointments[id],
-        interview: { ...interview }
-      };
-      const appointments = {
-        ...state.appointments,
-        [id]: appointment
-      };
-      
-      let updateModifier = (onUpdate === 'add' ? 1 : onUpdate === 'sub' ? -1 : 0 );
-      let remaining = getSpotsForDay(state, state.day).length + updateModifier;
-
-      const updateReamining = [...state.days].map(update => {
-        if (update.name === state.day) {
-          update = { ...update, spots: remaining };
-        }
-        return update;
-      });
-      
-      return { ...state, appointments, days: updateReamining };
-    }
-    default:
-      throw new Error(
-        `Tried to reduce with unsupported action type: ${action.type}`
-      );
-  }
-};
 
 export default function useApplicationData() {
 
