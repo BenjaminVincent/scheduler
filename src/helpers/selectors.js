265ad605daa18c5bfa6b267f7e50ebbbe3b5ff5d
BenjaminVincent <ben.tom.vincent@gmail.com>
2020-03-08 16:08:48 -0700

    updated getAppointmentsForDay function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,25 +1,11 @@
 export function getAppointmentsForDay (state, day) {
-  const fetchedApp = [];
-  const dayObj = state.days.filter(d => d.name === day)[0];
-  if (state.days < 1) {
-    return [];
-  }
-
-  if (!dayObj) {
-    return [];
-  }
-
-  for (const key of dayObj.appointments) {
-    if (state.appointments[key]) {
-      fetchedApp.push(state.appointments[key]);
-    }
-  }
-  return fetchedApp;
+  const dayObj = state.days.find((d) => d.name === day);
+  return dayObj ? dayObj.appointments.map((appointment) => state.appointments[appointment]) : [];
 }
 
 // will contain only the appointments where interview is null
 export function getSpotsForDay (state, day) {
-  return getAppointmentsForDay(state, day).filter(a => a.interview === null);;
+  return getAppointmentsForDay(state, day).filter(a => a.interview === null);
 };
 
 export function getInterviewerForDay (state, day) {
